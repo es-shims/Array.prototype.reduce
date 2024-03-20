4c68200a2ea24a919dfd3bd89c01953c34f85050
Jordan Harband <ljharb@gmail.com>
2024-03-20 14:42:00 -0700

    [Deps] update `call-bind`, `define-properties`, `es-abstract`

--- implementation.js
+++ implementation.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var Call = require('es-abstract/2023/Call');
-var Get = require('es-abstract/2023/Get');
-var HasProperty = require('es-abstract/2023/HasProperty');
-var IsCallable = require('es-abstract/2023/IsCallable');
-var LengthOfArrayLike = require('es-abstract/2023/LengthOfArrayLike');
+var Call = require('es-abstract/2024/Call');
+var Get = require('es-abstract/2024/Get');
+var HasProperty = require('es-abstract/2024/HasProperty');
+var IsCallable = require('es-abstract/2024/IsCallable');
+var LengthOfArrayLike = require('es-abstract/2024/LengthOfArrayLike');
 var ToObject = require('es-object-atoms/ToObject');
-var ToString = require('es-abstract/2023/ToString');
+var ToString = require('es-abstract/2024/ToString');
 
 var callBound = require('call-bind/callBound');
 var isString = require('is-string');
